1d9459f19ee56e93fc75f0db1aa6a09f476ccbe6
Cscharf91 <cscharf91@gmail.com>
2020-10-07 15:05:16 -0400

    Updates

---
+++ precache-manifest.62ac20fcfb10156911c291c4361c8143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7999ae0c6a8d21c115383ba26f1c7794",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "99fee98075696448fadf",
+    "url": "/memory-game/static/css/main.8249fe10.chunk.css"
+  },
+  {
+    "revision": "cda08bf0b2c9d2ccb32f",
+    "url": "/memory-game/static/js/2.98bf48f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.98bf48f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99fee98075696448fadf",
+    "url": "/memory-game/static/js/main.3941f0cc.chunk.js"
+  },
+  {
+    "revision": "cc5a158bb65db08dab34",
+    "url": "/memory-game/static/js/runtime-main.49b259c2.js"
+  }
+]);
